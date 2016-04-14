1752d28591a6b33a3bfe960838d1476cb231633e
Josbo12 <josepborras8@gmail.com>
2016-04-14 18:18:33 +0200

    aaa

--- task2.py
+++ task2.py
@@ -7,14 +7,14 @@ from datetime import datetime, date
 
 app = Flask(__name__)
 
-def save_data(zone,temp):
-    conn = sqlite3.connect('mydatabase.db')
+def save_data(username,name,password,email):
+    conn = sqlite3.connect('dadestask2.db')
     try:
-        conn.execute("insert into temps (tdate,ttime,zone,temperature) values (?, ?, ?, ?)",
-                 (date.today(),
-                  datetime.now().strftime("%H:%M:%S"),
-                  zone,
-                  temp))
+        conn.execute("insert into users (username,name,password,email) values (?, ?, ?, ?)",
+                 (username,
+                  name,
+                  password,
+                  email))
         conn.commit()
         conn.close()
         return True
@@ -23,25 +23,25 @@ def save_data(zone,temp):
 
 
 def get_data():
-    conn = sqlite3.connect('mydatabase.db')
-    cursor = conn.execute("SELECT tdate,ttime,zone,temperature from temps")
+    conn = sqlite3.connect('dadestask2.db')
+    cursor = conn.execute("SELECT username,name,password,email from users")
     data = [row for row in cursor]
     conn.close()
     return data
 
-def get_zone_data(zone):
-    conn = sqlite3.connect('mydatabase.db')
-    cursor = conn.execute("SELECT tdate,ttime,zone,temperature from temps WHERE zone = ?",(zone,))
-    data = [row for row in cursor]
-    conn.close()
-    return data
+#def get_zone_data(zone):
+#    conn = sqlite3.connect('dadestask2.db')
+#    cursor = conn.execute("SELECT tdate,ttime,zone,temperature from temps WHERE zone = ?",(zone,))
+#    data = [row for row in cursor]
+#    conn.close()
+#    return data
 
-def get_zones():
-    conn = sqlite3.connect('mydatabase.db')
-    cursor = conn.execute("select distinct zone from temps;")
-    data = [row[0] for row in cursor]
-    conn.close()
-    return data
+#def get_zones():
+#    conn = sqlite3.connect('dadestask2.db')
+#    cursor = conn.execute("select distinct zone from temps;")
+#    data = [row[0] for row in cursor]
+#    conn.close()
+#    return data
 
 @app.route('/')
 def hello():
@@ -52,30 +52,30 @@ def insert_user():
     if request.method == 'GET':
             return render_template('create_a_new_user.html')
     elif request.method == 'POST':
-        user = request.form.get('user')
+        user = request.form.get('username')
         name = request.form.get('name')
-        mail = request.form.get('mail')
-        password = request.form.get('password')
+        mail = request.form.get('password')
+        password = request.form.get('email')
         if save_data(zone,temp,mail,password):
             return redirect(url_for('hello'))
         else:
             return "Error inserting user"
 
-@app.route('/hist_data', methods=['GET', 'POST'])
-def hist_data():
-    historical_data = get_data()
-    return render_template('historical_data_table.html',historical_data=historical_data)
+@app.route('/list_of_users', methods=['GET', 'POST'])
+def list_of_users():
+    listusers = get_data()
+    return render_template('list_of_users.html',listusers=listusers)
 
-@app.route('/zone_data', methods=['GET', 'POST'])
-def zone_data():
-    zones = get_zones()
-    if request.method == 'GET':
-        zone_data = []
-    elif request.method == 'POST':
-        zone = request.form.get('area')
-        print(zone, file=sys.stderr)
-        zone_data = get_zone_data(zone)
-    return render_template('zone_data_table.html',zone_data=zone_data, zones=zones)
+#@app.route('/zone_data', methods=['GET', 'POST'])
+#def zone_data():
+#    zones = get_zones()
+#    if request.method == 'GET':
+#        zone_data = []
+#    elif request.method == 'POST':
+#        zone = request.form.get('area')
+#        print(zone, file=sys.stderr)
+#        zone_data = get_zone_data(zone)
+#    return render_template('zone_data_table.html',zone_data=zone_data, zones=zones)
 
 if __name__ == '__main__':
     app.debug = True
